d44109dd4b4dadbe5ac8777e694b327cbcdbe47b
Ankit Mittal <ankit.write@gmail.com>
2013-12-10 16:36:36 +0530

    First Commit

---
+++ js/sign.js
@@ -0,0 +1,35 @@
+var app = angular.module('leadformApp', []);
+
+app.controller('controller1', ['$scope', function($scope){
+	$scope.data = [];
+	$scope.submit_data = function(number, user){
+		$scope.data[number] = angular.copy(user);
+		$scope.sequence = $scope.data.length;
+	};
+	
+	$scope.edit = function(number){
+		$scope.user = angular.copy($scope.data[number]);
+		$scope.sequence = number; 
+		$scope.button_type = 'Modify';
+	};
+	
+	$scope.delete = function(number){
+		$scope.data.splice(number, 1);
+		$scope.sequence = $scope.data.length;
+		if (!$scope.sequence) $scope.showcase = false;
+	}
+	
+	$scope.isUnchanged = function(number, user){
+		var flag=true;
+		if($scope.data[number] == undefined){
+			return false
+		}
+		for(key in $scope.data[number]){
+			if($scope.data[number][key] != user[key] && key != '$$hashKey'){
+				flag=false;
+				break;
+			}
+		}
+		return flag
+	};
+}]);
